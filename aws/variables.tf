9736c754b19a505b7a317cad57e4059665415489
Prashant R <admin@ubuntu14-desktop>
2018-02-14 10:35:05 +0400

    Used customed Key-Pair wih ec2-user

--- aws/variables.tf
+++ aws/variables.tf
@@ -11,7 +11,7 @@ variable "ami" {
 
 variable "key_path" {
     description = "SSH Public Key path (generated)"
-    default = "/root/.ssh/id_brownie_rsa.pub"
+    default = "/ec2-user/.ssh/id_brownie_rsa.pub"
 }
  
 variable "vpc_cidr" {
