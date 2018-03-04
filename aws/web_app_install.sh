b4de552e90796195698694308d55b9e196224668
prashant <prashant@prashant-desktop>
2018-03-04 23:18:09 +0400

    Apps Installation and Docker Deploy

--- aws/web_app_install.sh
+++ aws/web_app_install.sh
@@ -13,6 +13,7 @@ systemctl start nginx
 yum install docker
 curl -L https://github.com/docker/compose/releases/download/1.9.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
 sudo chmod +x /usr/local/bin/docker-compose
+sudo usermod -a -G docker ec2-user
 
 # Starting Docker
 systemctl enable docker.service
